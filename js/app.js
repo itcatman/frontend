522b6457d51cf16d74287ce6c0bc3a034fc2641b
AliShah05 <alishahbazov2003@yandex.ru>
2021-04-10 18:32:47 +0300

    gopla

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const day1 = document.getElementById("1day");
-const day2 = document.querySelector("#2day");
-const day3 = document.querySelector("#3day");
-const day4 = document.querySelector("#4day");
-const day5 = document.querySelector("#5day");
-const course = document.querySelector(".course-ul")
-
